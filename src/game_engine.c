6b1c20ef49564c2ec5aa180dcc644332d5dc3702
stantheman0128 <stanshih888@gmail.com>
2024-06-17 05:09:25 +0800

    Merge branch 'main' of https://github.com/nrps9909/C_final_project

--- src/game_engine.c
+++ src/game_engine.c
@@ -4,135 +4,185 @@
 #include "game_engine.h"
 #include "ui_gui.h"
 
-void add_item_to_inventory(Player *player, Item *item) {
-    if (player->inventory_count < MAX_INVENTORY_SIZE) {
+void add_item_to_inventory(Player *player, Item *item)
+{
+    if (player->inventory_count < MAX_INVENTORY_SIZE)
+    {
         size_t name_length = strlen(item->name);
-        if (name_length >= MAX_ITEM_NAME_LENGTH) {
+        if (name_length >= MAX_ITEM_NAME_LENGTH)
+        {
             name_length = MAX_ITEM_NAME_LENGTH - 1;
         }
         strncpy(player->inventory[player->inventory_count], item->name, name_length);
-        player->inventory[player->inventory_count][name_length] = '\0';  // 確保字符串終止
+        player->inventory[player->inventory_count][name_length] = '\0'; // 確保字符串終止
         player->inventory_count++;
         printf("物品 %s 已添加到背包\n", item->name);
-    } else {
+    }
+    else
+    {
         printf("背包已滿,無法添加物品 %s\n", item->name);
     }
 }
 
-void update_emotion(Player *player, int change) {
+void update_emotion(Player *player, int change)
+{
     player->emotion += change;
-    if (player->emotion > 100) player->emotion = 100;
-    if (player->emotion < 0) player->emotion = 0;
+    if (player->emotion > 100)
+        player->emotion = 100;
+    if (player->emotion < 0)
+        player->emotion = 0;
 }
 
-void display_emotion(Player* player) {
+void display_emotion(Player *player)
+{
     printf("%s 的情感值: %d\n", player->name, player->emotion);
 }
 
-void display_inventory(Player *player) {
+void display_inventory(Player *player)
+{
     printf("背包內容:\n");
-    for (int i = 0; i < player->inventory_count; i++) {
+    for (int i = 0; i < player->inventory_count; i++)
+    {
         printf("%s\n", player->inventory[i]);
     }
 }
 
-int find_scene_index(GameData *gameData, const char *scene_name) {
-    for (int i = 0; i < MAX_SCENES; i++) {
-        if (strcmp(gameData->scenes[i].name, scene_name) == 0) {
+int find_scene_index(GameData *gameData, const char *scene_name)
+{
+    for (int i = 0; i < MAX_SCENES; i++)
+    {
+        if (strcmp(gameData->scenes[i].name, scene_name) == 0)
+        {
             return i;
         }
     }
     return -1;
 }
 
-int find_dialogue_index(GameData *gameData, const char *dialogue_name) {
-    for (int i = 0; i < MAX_DIALOGUES; i++) {
-        if (strcmp(gameData->dialogues[i].name, dialogue_name) == 0) {
+int find_dialogue_index(GameData *gameData, const char *dialogue_name)
+{
+    for (int i = 0; i < MAX_DIALOGUES; i++)
+    {
+        if (strcmp(gameData->dialogues[i].name, dialogue_name) == 0)
+        {
             return i;
         }
     }
     return -1;
 }
 
-int find_item_index(GameData *gameData, const char *item_name) {
-    for (int i = 0; i < MAX_ITEMS; i++) {
-        if (strcmp(gameData->items[i].name, item_name) == 0) {
+int find_item_index(GameData *gameData, const char *item_name)
+{
+    for (int i = 0; i < MAX_ITEMS; i++)
+    {
+        if (strcmp(gameData->items[i].name, item_name) == 0)
+        {
             return i;
         }
     }
     return -1;
 }
 
-int find_character_index(GameData *gameData, const char *character_name) {
-    for (int i = 0; i < MAX_CHARACTERS; i++) {
-        if (strcmp(gameData->characters[i].name, character_name) == 0) {
+int find_character_index(GameData *gameData, const char *character_name)
+{
+    for (int i = 0; i < MAX_CHARACTERS; i++)
+    {
+        if (strcmp(gameData->characters[i].name, character_name) == 0)
+        {
             return i;
         }
     }
     return -1;
 }
 
-void update_character_heart(GameData *gameData, const char *character_name, int amount) {
+void update_character_heart(GameData *gameData, const char *character_name, int amount)
+{
     int char_index = find_character_index(gameData, character_name);
-    if (char_index != -1) {
+    if (char_index != -1)
+    {
         gameData->characters[char_index].heart += amount;
         printf("更新 %s 的好感度: %d\n", character_name, gameData->characters[char_index].heart);
-    } else {
+    }
+    else
+    {
         printf("找不到角色: %s\n", character_name);
     }
 }
 
-void display_character_hearts(GameData *gameData) {
+void display_character_hearts(GameData *gameData)
+{
     printf("角色好感度:\n");
-    for (int i = 0; i < MAX_CHARACTERS; i++) {
-        if (strlen(gameData->characters[i].name) > 0) {
+    for (int i = 0; i < MAX_CHARACTERS; i++)
+    {
+        if (strlen(gameData->characters[i].name) > 0)
+        {
             printf("%s: %d\n", gameData->characters[i].name, gameData->characters[i].heart);
         }
     }
 }
 
-void handle_event(GameData *gameData, Player *player, const char *event_name) {
+void handle_event(GameData *gameData, Player *player, const char *event_name)
+{
     fprintf(stderr, "進入 handle_event 函數, 事件名稱: %s\n", event_name);
-    
+
     int found_event = 0;
-    for (int i = 0; i < MAX_EVENTS; i++) {
-        if (strcmp(gameData->events[i].name, event_name) == 0) {
+    for (int i = 0; i < MAX_EVENTS; i++)
+    {
+        if (strcmp(gameData->events[i].name, event_name) == 0)
+        {
             found_event = 1;
             fprintf(stderr, "事件名稱匹配: %s\n", gameData->events[i].name);
             fprintf(stderr, "事件動作: %s\n", gameData->events[i].action);
-            if (strcmp(gameData->events[i].action, "change_scene") == 0) {
+            if (strcmp(gameData->events[i].action, "change_scene") == 0)
+            {
                 fprintf(stderr, "事件動作是切換場景\n");
                 int scene_index = find_scene_index(gameData, gameData->events[i].scene);
-                if (scene_index != -1) {
+                if (scene_index != -1)
+                {
                     fprintf(stderr, "切換場景到: %s\n", gameData->events[i].scene);
                     player->current_scene = scene_index;
-                } else {
+                }
+                else
+                {
                     fprintf(stderr, "找不到場景: %s\n", gameData->events[i].scene);
                 }
-            } else if (strcmp(gameData->events[i].action, "add_item") == 0) {
+            }
+            else if (strcmp(gameData->events[i].action, "add_item") == 0)
+            {
                 fprintf(stderr, "事件動作是添加物品\n");
                 int item_index = find_item_index(gameData, gameData->events[i].item);
-                if (item_index != -1) {
+                if (item_index != -1)
+                {
                     fprintf(stderr, "找到物品: %s, 索引: %d\n", gameData->events[i].item, item_index);
                     add_item_to_inventory(player, &gameData->items[item_index]);
                     fprintf(stderr, "添加物品後的背包內容:\n");
                     display_inventory(player);
-                } else {
+                }
+                else
+                {
                     fprintf(stderr, "無效的物品名稱: %s\n", gameData->events[i].item);
                 }
-            } else if (strcmp(gameData->events[i].action, "update_heart") == 0) {
+            }
+            else if (strcmp(gameData->events[i].action, "update_heart") == 0)
+            {
                 fprintf(stderr, "事件動作是更新好感度\n");
                 update_character_heart(gameData, gameData->events[i].character, gameData->events[i].amount);
-            } else if (strcmp(gameData->events[i].action, "end_game") == 0) {
+            }
+            else if (strcmp(gameData->events[i].action, "end_game") == 0)
+            {
                 fprintf(stderr, "遊戲結束: %s\n", gameData->events[i].result);
                 exit(0); // 或者其他方式結束遊戲
             }
-            if (strlen(gameData->events[i].dialogue) > 0) {
+            if (strlen(gameData->events[i].dialogue) > 0)
+            {
                 int dialogue_index = find_dialogue_index(gameData, gameData->events[i].dialogue);
-                if (dialogue_index != -1) {
+                if (dialogue_index != -1)
+                {
                     fprintf(stderr, "更新對話到: %s\n", gameData->events[i].dialogue);
                     player->current_dialogue = dialogue_index;
-                } else {
+                }
+                else
+                {
                     fprintf(stderr, "找不到對話: %s\n", gameData->events[i].dialogue);
                 }
             }
@@ -140,7 +190,8 @@ void handle_event(GameData *gameData, Player *player, const char *event_name) {
         }
     }
 
-    if (!found_event) {
+    if (!found_event)
+    {
         fprintf(stderr, "找不到名為 %s 的事件\n", event_name);
     }
 
@@ -148,22 +199,26 @@ void handle_event(GameData *gameData, Player *player, const char *event_name) {
     fflush(stdout);
 }
 
-void play_game(GameData* gameData) {
+void play_game(GameData *gameData)
+{
     Player player = {"Player", 50, {{0}}, 0, 0, 0};
     player.current_scene = find_scene_index(gameData, "school-gate");
-    if (player.current_scene == -1) {
+    if (player.current_scene == -1)
+    {
         fprintf(stderr, "找不到初始場景: school-bus\n");
         return;
     }
 
     int dialogue_index = find_dialogue_index(gameData, "bus_introduction");
-    if (dialogue_index == -1) {
+    if (dialogue_index == -1)
+    {
         fprintf(stderr, "找不到初始對話: bus_introduction\n");
         return;
     }
     player.current_dialogue = dialogue_index;
 
-    while (1) {
+    while (1)
+    {
         fprintf(stderr, "當前場景: %s\n", gameData->scenes[player.current_scene].name);
         display_scene(gameData, gameData->scenes[player.current_scene].name, &player);
         fprintf(stderr, "顯示對話之前\n");
@@ -173,54 +228,81 @@ void play_game(GameData* gameData) {
         int choice = get_user_choice();
         fprintf(stderr, "用戶選擇: %d\n", choice);
 
-        if (choice == -1) {
+        if (choice == -1)
+        {
             break;
         }
 
-        if (choice == 0) {
+        if (choice == 0)
+        {
             display_inventory_screen(gameData, &player);
             continue;
         }
 
         DialogueOption selected_option;
-        if (choice == 1) {
+        if (choice == 1)
+        {
             selected_option = gameData->dialogues[player.current_dialogue].options[0];
-        } else if (choice == 2) {
+        }
+        else if (choice == 2)
+        {
             selected_option = gameData->dialogues[player.current_dialogue].options[1];
-        } else {
+        }
+        else if (choice == 3)
+        {
+            selected_option = gameData->dialogues[player.current_dialogue].options[2];
+        }
+        else if (choice == 4)
+        {
+            selected_option = gameData->dialogues[player.current_dialogue].options[3];
+        }
+        else
+        {
             fprintf(stderr, "無效選擇\n");
             continue;
         }
 
         fprintf(stderr, "選中的對話選項: %s\n", selected_option.text);
 
-        if (strlen(selected_option.event) > 0) {
+        if (strlen(selected_option.event) > 0)
+        {
             fprintf(stderr, "處理事件: %s\n", selected_option.event);
             handle_event(gameData, &player, selected_option.event);
-        } else {
+        }
+        else
+        {
             fprintf(stderr, "選項沒有關聯的事件\n");
         }
 
-        if (strlen(selected_option.next) > 0) {
+        if (strlen(selected_option.next) > 0)
+        {
             const char *next_dialogue = selected_option.next;
             fprintf(stderr, "查找對話: %s\n", next_dialogue);
             int next_dialogue_index = find_dialogue_index(gameData, next_dialogue);
-            if (next_dialogue_index != -1) {
+            if (next_dialogue_index != -1)
+            {
                 fprintf(stderr, "更新對話到: %s\n", next_dialogue);
                 player.current_dialogue = next_dialogue_index;
                 const char *next_scene = gameData->dialogues[next_dialogue_index].scene;
                 int next_scene_index = find_scene_index(gameData, next_scene);
-                if (next_scene_index != -1) {
+                if (next_scene_index != -1)
+                {
                     player.current_scene = next_scene_index;
                     fprintf(stderr, "更新場景到: %s\n", next_scene);
-                } else {
+                }
+                else
+                {
                     fprintf(stderr, "找不到下一個對話的場景: %s\n", next_scene);
                 }
-            } else {
+            }
+            else
+            {
                 fprintf(stderr, "找不到對話: %s\n", next_dialogue);
                 break;
             }
-        } else {
+        }
+        else
+        {
             fprintf(stderr, "選項的下一個對話為空\n");
         }
     }
--- src/game_engine.h
+++ src/game_engine.h
@@ -13,49 +13,56 @@
 #define MAX_EVENTS 50
 
 // 定義玩家結構體
-typedef struct {
-    char name[MAX_ITEM_NAME_LENGTH]; // 玩家名稱
-    int emotion; // 玩家情緒值
+typedef struct
+{
+    char name[MAX_ITEM_NAME_LENGTH];                          // 玩家名稱
+    int emotion;                                              // 玩家情緒值
     char inventory[MAX_INVENTORY_SIZE][MAX_ITEM_NAME_LENGTH]; // 玩家物品清單
-    int inventory_count; // 玩家物品數量
-    int current_scene; // 當前場景
-    int current_dialogue; // 當前對話
+    int inventory_count;                                      // 玩家物品數量
+    int current_scene;                                        // 當前場景
+    int current_dialogue;                                     // 當前對話
 } Player;
 
 // 定義場景結構體
-typedef struct {
-    char name[50]; // 場景名稱
+typedef struct
+{
+    char name[50];        // 場景名稱
     char background[100]; // 場景背景
 } Scene;
 
 // 定義角色結構體
-typedef struct {
-    char name[50]; // 角色名稱
-    char avatar[100]; // 角色頭像
-    char tachie[100]; // 角色立繪
+typedef struct
+{
+    char name[50];     // 角色名稱
+    char avatar[100];  // 角色頭像
+    char tachie[100];  // 角色立繪
     char location[50]; // 角色位置
-    int heart; // 角色好感度
+    int heart;         // 角色好感度
 } Character;
 
 // 定義物品結構體
-typedef struct {
-    char name[50]; // 物品名稱
+typedef struct
+{
+    char name[50];         // 物品名稱
     char description[200]; // 物品描述
-    char icon[100]; // 物品圖標
-    char scene[50]; // 物品所屬場景
+    char icon[100];        // 物品圖標
+    char scene[50];        // 物品所屬場景
 } Item;
 
 // 定義對話選項結構體
-typedef struct {
-    char text[200]; // 對話選項文本
-    char next[50]; // 下一個對話名稱
-    char event[50]; // 觸發事件
+typedef struct
+{
+    char text[200];  // 對話選項文本
+    char next[50];   // 下一個對話名稱
+    char event[50];  // 觸發事件
+    char action[50]; // Add this line to include the action field
 } DialogueOption;
 
 // 定義對話結構體
-typedef struct {
-    char name[50]; // 對話名稱
-    char scene[50]; // 對話所屬場景
+typedef struct
+{
+    char name[50];      // 對話名稱
+    char scene[50];     // 對話所屬場景
     char character[50]; // 參與對話的角色
     char text1[256];
     char text2[256];
@@ -64,38 +71,40 @@ typedef struct {
 } Dialogue;
 
 // 定義事件結構體
-typedef struct {
-    char name[50]; // 事件名稱
-    char scene[50]; // 事件所屬場景
-    char dialogue[50]; // 事件所屬對話
-    char action[50]; // 事件動作
-    char result[50]; // 事件結果
-    char item[50]; // 事件物品
+typedef struct
+{
+    char name[50];      // 事件名稱
+    char scene[50];     // 事件所屬場景
+    char dialogue[50];  // 事件所屬對話
+    char action[50];    // 事件動作
+    char result[50];    // 事件結果
+    char item[50];      // 事件物品
     char character[50]; // 事件角色
-    int amount; // 事件影響的數量
+    int amount;         // 事件影響的數量
 } Event;
 
 // 定義遊戲資料結構體
-typedef struct {
-    Scene scenes[MAX_SCENES]; // 所有場景
+typedef struct
+{
+    Scene scenes[MAX_SCENES];             // 所有場景
     Character characters[MAX_CHARACTERS]; // 所有角色
-    Item items[MAX_ITEMS]; // 所有物品
-    Dialogue dialogues[MAX_DIALOGUES]; // 所有對話
-    Event events[MAX_EVENTS]; // 所有事件
+    Item items[MAX_ITEMS];                // 所有物品
+    Dialogue dialogues[MAX_DIALOGUES];    // 所有對話
+    Event events[MAX_EVENTS];             // 所有事件
 } GameData;
 
 // 函式原型
-void play_game(GameData *gameData); // 遊戲主函式
-void handle_event(GameData *gameData, Player *player, const char *event_name); // 處理事件
-int find_scene_index(GameData *gameData, const char *scene_name); // 找到場景索引
-int find_dialogue_index(GameData *gameData, const char *dialogue_name); // 找到對話索引
-int find_item_index(GameData *gameData, const char *item_name); // 找到物品索引
-int find_character_index(GameData *gameData, const char *character_name); // 找到角色索引
-void add_item_to_inventory(Player *player, Item *item); // 將物品加入玩家物品欄
-void update_emotion(Player *player, int change); // 更新玩家情緒
-void display_emotion(Player *player); // 顯示玩家情緒
-void display_inventory(Player *player); // 顯示玩家物品欄
+void play_game(GameData *gameData);                                                      // 遊戲主函式
+void handle_event(GameData *gameData, Player *player, const char *event_name);           // 處理事件
+int find_scene_index(GameData *gameData, const char *scene_name);                        // 找到場景索引
+int find_dialogue_index(GameData *gameData, const char *dialogue_name);                  // 找到對話索引
+int find_item_index(GameData *gameData, const char *item_name);                          // 找到物品索引
+int find_character_index(GameData *gameData, const char *character_name);                // 找到角色索引
+void add_item_to_inventory(Player *player, Item *item);                                  // 將物品加入玩家物品欄
+void update_emotion(Player *player, int change);                                         // 更新玩家情緒
+void display_emotion(Player *player);                                                    // 顯示玩家情緒
+void display_inventory(Player *player);                                                  // 顯示玩家物品欄
 void update_character_heart(GameData *gameData, const char *character_name, int amount); // 更新角色好感度
-void display_character_hearts(GameData *gameData); // 顯示角色好感度
+void display_character_hearts(GameData *gameData);                                       // 顯示角色好感度
 
 #endif // GAME_ENGINE_H
